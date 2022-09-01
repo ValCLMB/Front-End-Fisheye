d7365df31499cc4962572d669d480ba253e2cf4c
ValCLMB <valentin.coulomb2@gmail.com>
2022-09-01 10:43:42 +0200

    create new files for picturesFactory

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -101,25 +101,3 @@ function photographerFactory(data) {
 
   return { name, picture, getUserCardDOM, getHeaderDOM, getAdditionalInfosDOM };
 }
-
-function picturesFactory(picture, photographerName) {
-  const { date, likes, price, image } = picture;
-
-  console.log(picture);
-  const name = photographerName.split(" ")[0];
-
-  // pictures card
-  function getPicturesCardDOM() {
-    const article = document.createElement("article");
-    // img
-    const img = document.createElement("img");
-    img.setAttribute("src", `/assets/photographers/${name}/${image}`);
-    const descr = document.createElement("div");
-
-    article.appendChild(img);
-    article.appendChild(descr);
-    return article;
-  }
-
-  return { picture, getPicturesCardDOM };
-}
