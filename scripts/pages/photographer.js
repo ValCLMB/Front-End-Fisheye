47e3b144b4e67463cfeb8542c928a2c20c3d61d3
ValCLMB <valentin.coulomb2@gmail.com>
2022-09-01 10:19:54 +0200

    start to create picturecardDOM, fix json fetch for pictures Add heart svg

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,30 +1,39 @@
 //Mettre le code JavaScript lié à la page photographer.html
 async function getPhotographerPictures(id) {
-    let pictures = [];
-    fetch('../../data/photographers.json')
-        .then(res => res.json(res))
-        .then(json => json.media.forEach(picture => {
-            if(picture.photographerId === id) pictures.push(picture)
-        }))
-    return pictures;
+  let pictures = [];
+  await fetch("../../data/photographers.json")
+    .then((res) => res.json())
+    .then((json) => {
+      json.media.forEach((picture) => {
+        if (picture.photographerId === id) pictures.push(picture);
+      });
+    });
+  return pictures;
 }
-async function displayData(photographer) {
-    const main = document.querySelector("main")
-    const header = document.querySelector(".photograph-header")
+async function displayData(datas) {
+  const main = document.querySelector("main");
+  const header = document.querySelector(".photograph-header");
 
-    const photographerModel = photographerFactory(photographer.photographer);
-    // header
-    const headerDOM = photographerModel.getHeaderDOM();
-    header.prepend(headerDOM.infos);
-    header.append(headerDOM.img)
-    // additional infos (like, price)
-    const additionalInfosDOM = photographerModel.getAdditionalInfosDOM();
-    main.append(additionalInfosDOM)
+  const photographerModel = photographerFactory(datas.photographer);
+  // header
+  const headerDOM = photographerModel.getHeaderDOM();
+  header.prepend(headerDOM.infos);
+  header.append(headerDOM.img);
+  // additional infos (like, price)
+  const additionalInfosDOM = photographerModel.getAdditionalInfosDOM();
+  main.append(additionalInfosDOM);
+
+  // photos
+  const photoSection = document.querySelector(".photograph-photos");
+
+  //   datas.pictures.forEach((pic) => {
+  //     const pictureModel = picturesFactory(picture);
+  //   });
 }
 
 async function init() {
-    const photographer = JSON.parse(window.sessionStorage.getItem("profil"));
-    const pictures = getPhotographerPictures(photographer.id);
-    displayData({photographer, pictures})
+  const photographer = JSON.parse(window.sessionStorage.getItem("profil"));
+  const pictures = await getPhotographerPictures(photographer.id);
+  displayData({ photographer, pictures });
 }
 init();
