ac6fd7e61b47dc8d06ca6af36d0f7fc18462ffa3
ValCLMB <valentin.coulomb2@gmail.com>
2022-09-01 11:18:52 +0200

    Display description with style, and display video

--- scripts/factories/pictures.js
+++ scripts/factories/pictures.js
@@ -1,21 +1,51 @@
 function picturesFactory(picture, photographerName) {
-  const { date, likes, price, image } = picture;
-
   console.log(picture);
+  const { date, likes, price, image, title, video } = picture;
+
   const name = photographerName.split(" ")[0];
 
   // pictures card
   function getPicturesCardDOM() {
     const article = document.createElement("article");
-    // img
-    const img = document.createElement("img");
-    img.setAttribute("src", `/assets/photographers/${name}/${image}`);
-    const descr = document.createElement("div");
+    // img - video
+    const path = `/assets/photographers/${name}/${image ? image : video}`;
+    let img, vid;
 
-    article.appendChild(img);
+    if (image) {
+      img = document.createElement("img");
+      img.setAttribute("src", path);
+    } else {
+      vid = document.createElement("video");
+      vid.classList.add("video");
+      const src = document.createElement("source");
+      src.setAttribute("src", path);
+      src.setAttribute("type", "video/mp4");
+      vid.appendChild(src);
+    }
+
+    // descr
+    const descr = document.createElement("div");
+    descr.classList.add("descr");
+
+    const paragraphClasses = [
+      { name: "title", value: title },
+      {
+        name: "likes",
+        value: `${likes}<img src="/assets/icons/heart.svg" alt="like" class="likes-img"/>`,
+      },
+    ];
+
+    paragraphClasses.forEach((p) => {
+      let infos = document.createElement("p");
+      infos.setAttribute("class", p.name);
+      infos.innerHTML = p.value;
+      descr.appendChild(infos);
+    });
+
+    article.appendChild(image ? img : vid);
     article.appendChild(descr);
     return article;
   }
 
-  return { picture, getPicturesCardDOM };
+  return { getPicturesCardDOM };
 }
