8fcde2f23c8e23b2a3169e996e449e124dd0be80
ValCLMB <valentin.coulomb2@gmail.com>
2022-09-02 13:45:42 +0200

    Make form and submit function

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,28 @@
 function displayModal() {
-    const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+  const modal = document.getElementById("contact_modal");
+  const title = document.querySelector(".modal header h2");
+  const name = JSON.parse(window.sessionStorage.getItem("profil")).name;
+
+  title.innerHTML = `Contactez moi <span class="modal-photographer-name">${name}</span>`;
+  modal.style.display = "block";
 }
 
 function closeModal() {
-    const modal = document.getElementById("contact_modal");
-    modal.style.display = "none";
+  const modal = document.getElementById("contact_modal");
+  modal.style.display = "none";
+}
+
+function submitForm() {
+  const firstName = document.querySelector("#contact-firstname").value;
+  const lastName = document.querySelector("#contact-lastname").value;
+  const email = document.querySelector("#contact-email").value;
+  const message = document.querySelector("#contact-message").value;
+
+  const options = {
+    firstname: firstName,
+    lastname: lastName,
+    email: email,
+    msg: message,
+  };
+  console.log(options);
 }
